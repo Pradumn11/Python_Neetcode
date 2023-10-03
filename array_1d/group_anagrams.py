2525ac819a201ecc4f986c2b66f8af737a20599f
Pradumn <pradumnyajr@gmail.com>
2023-10-03 23:53:56 +0100

    group Anagram

---
+++ array_1d/group_anagrams.py
@@ -0,0 +1,18 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of
+# a different word or phrase, typically using all the original letters exactly once.
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+
+        hashmap = defaultdict(list)
+        for s in strs:
+            chars = [0] * 26
+
+            for c in s:
+                chars[ord(c) - ord('a')] += 1
+
+            hashmap[tuple(chars)].append(s)
+        return hashmap.values()
