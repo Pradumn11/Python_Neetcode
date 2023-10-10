b2e359dd276df11e52702a84c87b097de2261108
Pradumn <pradumnyajr@gmail.com>
2023-10-10 23:09:53 +0100

    Evaluate_reveral_notation

--- Evaluate_reveral_notation.py
+++ Evaluate_reveral_notation.py
@@ -1,3 +1,18 @@
+# Evaluate Reverse Polish Notation
+#
+# The valid operators are '+', '-', '*', and '/'.
+# Each operand may be an integer or another expression.
+# The division between two integers always truncates toward zero.
+# There will not be any division by zero.
+# The input represents a valid arithmetic expression in a reverse polish notation.
+# The answer and all the intermediate calculations can be represented in a 32-bit integer.
+#
+# Example 1:
+# Input: tokens = ["2","1","+","3","*"]
+# Output: 9
+# Explanation: ((2 + 1) * 3) = 9
+
+
 class Solution(object):
 
     def evalRPN(self, tokens):
